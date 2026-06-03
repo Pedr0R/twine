3021db2ed62460bac816d5810949da642faf82f2
Pedro Ramos <pedroramos327698@gmail.com>
2026-06-03 19:26:22 -0300

    log newTab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -254,7 +254,8 @@ export class AppComponent implements OnInit {
       selectedItem,
       isDirty: false
     };
-
+    console.log(newTab);
+    
     // Parse query params if url contains them
     if (config?.url) {
       try {
